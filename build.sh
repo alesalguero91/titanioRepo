dd7eae0eac342a1d9481c4b9e4e5a3271a368bec
Alejandro <nestorale.salguero91@outlook.es>
2025-07-11 12:43:16 -0300

    otraVeks

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Instala Tesseract (requiere comunidad Render)
+apt-get update && apt-get install -y --no-install-recommends \
+    tesseract-ocr \
+    tesseract-ocr-spa \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+# Instala dependencias Python
 pip install -r requirements.txt
 
+# Colecta archivos estáticos
 python manage.py collectstatic --no-input
-python manage.py migrate
