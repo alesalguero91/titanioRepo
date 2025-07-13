988d632c5631d12e07fbea64fa74b26f2cee7bad
Alejandro <nestorale.salguero91@outlook.es>
2025-07-12 22:15:17 -0300

    agregando cors

--- titanio2/settings.py
+++ titanio2/settings.py
@@ -50,9 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lectorpdf',
     'rest_framework',
+    'corsheaders',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
